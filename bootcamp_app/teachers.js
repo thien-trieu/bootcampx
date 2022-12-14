d46590b221b6f60b7dfc11583da31ae3db6a435c
Thien Trieu <thien.trieu@telus.com>
2022-12-13 21:03:51 -0800

    update query for students and teachers to use parameterized query to prevent sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,41 @@ const pool = new Pool({
 });
 
 
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
 
-pool.query(`
+// Parameterized Query
+// $ node teachers.js JUL02
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+
+pool.query(queryString, values)
 .then(res => {
+  console.log(res)
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
